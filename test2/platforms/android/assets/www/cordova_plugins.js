41134fe8a16886f489d60347f376a270bb8a9ff4
walaa111 <walaagamil18@gmail.com>
2017-11-14 17:02:16 +0200

    s

--- test2/platforms/android/assets/www/cordova_plugins.js
+++ test2/platforms/android/assets/www/cordova_plugins.js
@@ -7,13 +7,23 @@ module.exports = [
         "clobbers": [
             "OneSignal"
         ]
+    },
+    {
+        "id": "cordova-plugin-admob-simple.AdMob",
+        "file": "plugins/cordova-plugin-admob-simple/www/AdMob.js",
+        "pluginId": "cordova-plugin-admob-simple",
+        "clobbers": [
+            "window.plugins.AdMob"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "onesignal-cordova-plugin": "2.2.2",
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-admob-sdklibs": "2.1.6",
+    "cordova-plugin-admob-simple": "3.3.4"
 };
 // BOTTOM OF METADATA
 });
--- test2/www/js/index.js
+++ test2/www/js/index.js
@@ -49,7 +49,61 @@ document.addEventListener('deviceready', function () {
   // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
   // window.plugins.OneSignal.syncHashedEmail(userEmail);
 }, false);
-    },
+   
+///ads
+
+//initialize the goodies
+function initAd(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-7251676025279948/7827344712',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-7251676025279948/5792208319'	//PUT ADMOB ADCODE HERE
+                },
+                android : {
+                    banner: 'ca-app-pub-7251676025279948/7827344712',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-7251676025279948/5792208319'	//PUT ADMOB ADCODE HERE
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD
+                bannerAtTop: false, // set to true, to put banner at top
+                overlap: true, // banner will overlap webview 
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+                isTesting: false, // receiving test ad
+                autoShow: false // auto show interstitial ad when loaded
+            });
+
+            registerAdEvents();
+            window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown
+            window.plugins.AdMob.requestInterstitialAd();
+
+        } else {
+            //alert( 'admob plugin not ready' );
+        }
+}
+//functions to allow you to know when ads are shown, etc.
+function registerAdEvents() {
+        document.addEventListener('onReceiveAd', function(){});
+        document.addEventListener('onFailedToReceiveAd', function(data){});
+        document.addEventListener('onPresentAd', function(){});
+        document.addEventListener('onDismissAd', function(){ });
+        document.addEventListener('onLeaveToAd', function(){ });
+        document.addEventListener('onReceiveInterstitialAd', function(){ });
+        document.addEventListener('onPresentInterstitialAd', function(){ });
+        document.addEventListener('onDismissInterstitialAd', function(){
+        	window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW
+            window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed
+        });
+    }
+
+    //end ads
+
+ },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
